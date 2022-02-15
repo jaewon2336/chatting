69e1b623f0ccdb9d8c5d1afa0baa0fc845f1c26b
jaewon2336 <xldzjqpf1588@naver.com>
2022-02-15 11:05:59 +0900

    2. 전이중 통신

--- src/site/metacoding/chat/MyClientSocket.java
+++ src/site/metacoding/chat/MyClientSocket.java
@@ -1,26 +1,65 @@
 package site.metacoding.chat;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.Scanner;
+
+import javax.swing.event.SwingPropertyChangeSupport;
 
 public class MyClientSocket {
 
     Socket socket;
     BufferedWriter writer;
+    BufferedReader reader;
+    Scanner scan;
+    String getMsg;
 
     public MyClientSocket() {
         try {
+
+            // 스캐너 달고(반복x), 키보드로부터 입력 받는 부분(반복o)
+            scan = new Scanner(System.in);
+
             // IP주소, 포트번호
             // 서버소켓과 연결
-            socket = new Socket("192.168.0.132", 1077);
+            socket = new Socket("localhost", 1077);
 
             writer = new BufferedWriter(
                     new OutputStreamWriter(socket.getOutputStream()));
 
-            // 마지막 메세지 끝에는 \n이 꼭 필요함
-            writer.write("정재원 \n"); // 버퍼에 담김
-            writer.flush(); // 버퍼가 가득 차지 않았기 때문에 비워줘야 함
+            reader = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+
+            // 메시지 읽기
+            new Thread(() -> {
+                while (true) {
+                    try {
+                        String inputData = reader.readLine();
+                        System.out.println("서버 -> 클라이언트 : " + inputData);
+
+                        if (inputData.equals("aaa")) {
+                            break;
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+            while (true) {
+                // 순차적이라서 스레드 필요 없음
+                getMsg = scan.nextLine();
+
+                // 마지막 메세지 끝에는 \n이 꼭 필요함
+                writer.write("Msg : " + getMsg + " \n"); // 버퍼에 담김
+                writer.flush(); // 버퍼가 가득 차지 않았기 때문에 비워줘야 함
+
+                if (getMsg.equals("aaa"))
+                    break;
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/site/metacoding/chat/MyServerSocket.java
+++ src/site/metacoding/chat/MyServerSocket.java
@@ -1,16 +1,24 @@
 package site.metacoding.chat;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class MyServerSocket {
 
     // OS의 socket 라이브러리 기반 System call
-    ServerSocket serverSocket; // 연결하는지 지켜보는 리스너(연결 => 세션?)
+    ServerSocket serverSocket; // 연결하는지 지켜보는 리스너(연결 => 세션이 만들어짐 => 인증됨!)
     Socket socket; // 메시지 통신
     BufferedReader reader;
+
+    // 추가(클라이언트에게 메시지 보내기)
+    BufferedWriter writer;
+    Scanner scan;
 
     public MyServerSocket() {
         // 통신은 무조건 예외가 발생할 수 있음!
@@ -19,19 +27,51 @@ public class MyServerSocket {
             // 잘 알려진 포트 : 0 ~ 1023
             serverSocket = new ServerSocket(1077); // 내부적으로 while이 돈다.
             System.out.println("서버 소켓 생성됨");
+            System.out.println("aaa를 입력하면 종료됩니다.");
 
             // 종료되지 않는 프로그램 -> 데몬(main 스레드)
             // 내부적으로 accept가 새로운 소켓을 만들어서 return하여 연결해줌
             // 이때 소켓의 포트번호는 사용하지 않는 포트 랜덤지정(OS에게 맡김)
+            // 포트번호 0 ~ 65536
             socket = serverSocket.accept(); // while을 돌면서 대기, 실제로 while 도는 메서드
 
+            // 새로운 소켓을 알아야 버퍼를 달고 통신, 순서 중요
             reader = new BufferedReader(
                     new InputStreamReader(socket.getInputStream()) // socket 선 = conn
             );
 
-            String inputData = reader.readLine();
-            System.out.println("받은 메시지 : " + inputData);
-            System.out.println("클라이언트 연결됨");
+            // 메시지 보내기
+            scan = new Scanner(System.in);
+            writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+            new Thread(() -> {
+                // 익명 클래스 : 다른 공간이라서 try-catch 따로 걸어줘야함
+                while (true) {
+                    try {
+                        String inputData = scan.nextLine();
+                        writer.write(inputData + "\n");
+                        writer.flush();
+
+                        if (inputData.equals("aaa")) {
+                            break;
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+            // 메시지 반복해서 받는 서버 소켓 - main 스레드
+            while (true) {
+                // System.out.println("클라이언트 연결됨");
+                String inputData = reader.readLine();
+                System.out.println("클라이언트 -> 서버 : " + inputData);
+
+                if (inputData.equals("aaa")) {
+                    break;
+                }
+            }
+
         } catch (Exception e) {
             System.out.println("통신 오류 발생 : " + e.getMessage());
             // e.printStackTrace();
